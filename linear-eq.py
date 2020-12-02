05000b340a6a81a025ebf70201f0198360b76ea7
Jaskirat_sudan <jaskiratsinghsudan@gmail.com>
2020-12-02 22:23:22 +0530

    Version 1

--- linear-eq.py
+++ linear-eq.py
@@ -59,56 +59,6 @@ def solve(nov):
         print(f"{var[i]}={ans[i]}")
 
 
-    # coeff = []
-
-    # for i in eq:
-
-    #     i = i.replace(" ","")
-
-    #     cox = i[:i.index("x")]
-    #     coy = i[i.index("y")-2:i.index("y")]
-    #     coz = i[i.index("z")-2:i.index("z")]
-
-    #     coeff.append(cox)
-    #     coeff.append(coy)
-    #     coeff.append(coz)
-    
-    # coeff = np.array(coeff).reshape((3,3))
-    # coeff = coeff.astype(np.int)
-    # return(coeff)
-
-    # def const(*eq):
-
-    #     const = []
-
-    #     for i in eq:
-
-    #         i = i.replace(" ","")
-
-    #         con = i[i.index("=")+1:]
-        
-    #         const.append(con)
-
-    #     const = np.array(const).reshape((3,1))
-    #     const = const.astype(np.int)
-    #     return(const)
-
-    # def sol(coeff,const):
-
-    #     ans = np.dot(np.linalg.inv(coeff), const)
-    #     ans = ans.tolist()
-
-    #     x = ans[0][0]
-    #     y = ans[1][0]
-    #     z = ans[2][0]
-
-    #     print(f"x = {int(x)}\ny = {int(y)}\nz = {int(z)}")
-
-#        print(np.linalg.inv(coeff))
-
-    # print(f"Solution :")
-    # sol(coeff(*eq), const(*eq))
-
 
 
 
